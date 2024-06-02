1b8608501c99e6251f4dc5aa3b26791c9e7daf57
yangbo <yangbo@movee.cn>
2024-06-02 15:30:36 +0800

    change: lib name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'sdp-vue-prefetch.js',
     library: 'sdp-vue-prefetch',
     libraryTarget: 'umd',
     umdNamedDefine: true
