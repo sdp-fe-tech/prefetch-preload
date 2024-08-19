97aa95af91497af078e874d8a5c4b30f0170b190
lns567 <1203207252@qq.com>
2024-08-19 20:19:28 +0800

    feat: 读取路由组件映射关系更改为读取指定配置文件

--- src/plugins/webpack-route-map/index.ts
+++ src/plugins/webpack-route-map/index.ts
@@ -67,21 +67,65 @@ export class RouteMappingPlugin {
           const routes: { path?: string; component?: string }[] = [];
 
           traverse(ast, {
-            NewExpression(path) {
-              if (
-                (path.node.callee as any).name === 'Router' &&
-                path.node.arguments.length > 0 &&
-                path.node.arguments[0].type === 'ObjectExpression'
-              ) {
-                const routesProperty = path.node.arguments[0].properties.find(
-                  (prop: any) => prop.key.name === 'routes',
-                );
-
-                if (
-                  routesProperty &&
-                  (routesProperty as any).value.type === 'ArrayExpression'
-                ) {
-                  (routesProperty as any).value.elements.forEach((routeNode: any) => {
+            // NewExpression(path) {
+            //   if (
+            //     (path.node.callee as any).name === 'Router' &&
+            //     path.node.arguments.length > 0 &&
+            //     path.node.arguments[0].type === 'ObjectExpression'
+            //   ) {
+            //     const routesProperty = path.node.arguments[0].properties.find(
+            //       (prop: any) => prop.key.name === 'routes',
+            //     );
+            //
+            //     if (
+            //       routesProperty &&
+            //       (routesProperty as any).value.type === 'ArrayExpression'
+            //     ) {
+            //       (routesProperty as any).value.elements.forEach((routeNode: any) => {
+            //         if (routeNode.type === 'ObjectExpression') {
+            //           const route: { path?: string; component?: string } = {};
+            //           routeNode.properties.forEach((prop: any) => {
+            //             if (prop.key.name === 'path') {
+            //               route.path = prop.value.value;
+            //             }
+            //             if (prop.key.name === 'component') {
+            //               if (
+            //                 prop.value.type === 'ArrowFunctionExpression' &&
+            //                 dynamicImports.has(prop)
+            //               ) {
+            //                 route.component = dynamicImports.get(prop);
+            //               } else if (prop.value.type === 'Identifier') {
+            //                 const componentName = prop.value.name;
+            //                 const mapping = routeMappings.find(
+            //                   (m) => m.componentName === componentName,
+            //                 );
+            //                 if (mapping) {
+            //                   route.component = mapping.importPath;
+            //                 }
+            //               }
+            //             }
+            //           });
+            //           if (route.path && route.component) {
+            //             routes.push(route);
+            //           }
+            //         }
+            //       });
+            //     }
+            //   }
+            // },
+            Declaration(path) {
+              // 类型为VariableDeclaration且参数名为prefetchRoutes
+              if (path.node.type === 'VariableDeclaration' && (path.node.declarations[0].id as any).name === 'prefetchRoutes') {
+                console.log(path.node);
+                //  const prefetchRoutes = [
+                //   {
+                //     path: '/goods/list',
+                //     component: () => import('@/pages/goods/list'),
+                //   }
+                // ] 取到里面每一个对象的path和component
+                if (path.node.declarations[0].init && path.node.declarations[0].init.type === 'ArrayExpression') {
+                  const prefetchRoutes = path.node.declarations[0].init.elements;
+                  (prefetchRoutes).forEach((routeNode: any) => {
                     if (routeNode.type === 'ObjectExpression') {
                       const route: { path?: string; component?: string } = {};
                       routeNode.properties.forEach((prop: any) => {
@@ -112,8 +156,9 @@ export class RouteMappingPlugin {
                   });
                 }
               }
-            },
+            }
           });
+          console.log(routes, '路由');
           const code = `window.__sdp_base_route_maps = ${JSON.stringify(routes)}`;
           compilation.assets[outputFile] = {
             source: () => code,
