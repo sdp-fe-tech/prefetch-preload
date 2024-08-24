9f3580bc8f9a6cf3c6e25b8e8b349770efed4867
yangbo <yangbo@movee.cn>
2024-08-24 17:40:02 +0800

    feat:  webpack-route-map 改写

--- src/plugins/webpack-route-map/index.ts
+++ src/plugins/webpack-route-map/index.ts
@@ -48,132 +48,130 @@ export class RouteMappingPlugin {
   }
 
   apply(compiler: Compiler) {
-    let hashedFilename;
+    let hashedFilename: string;
 
-    compiler.hooks.compilation.tap('MyPlugin', (compilation) => {
+
+    compiler.hooks.compilation.tap('RouteMappingPlugin', (compilation) => {
+
       const HtmlWebpackPlugin = safeRequire('html-webpack-plugin')
-
       HtmlWebpackPlugin.getHooks(compilation).beforeAssetTagGeneration.tapAsync(
-        'MyPlugin',
-        (data: { plugin: { options: { chunks: string[]; }; }; }, cb: (arg0: null, arg1: any) => void) => {
-          // 确保将 route-mapping 加入到 chunks 中
-          if (!data.plugin.options.chunks.includes('route-mapping')) {
-            data.plugin.options.chunks.push('route-mapping');
-          }
+        'RouteMappingPlugin',
+        (data: { assets: { js: any[]; }; }, cb: (arg0: null, arg1: any) => void) => {
+          data.assets.js.unshift(hashedFilename)
           cb(null, data);
         },
       );
 
-      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
-        'MyPlugin',
-        (data: { headTags: { tagName: string; voidTag: boolean; attributes: { src: string; }; }[]; }, cb: (arg0: null, arg1: any) => void) => {
-          // 动态添加 route-mapping.js 到 script 标签中
-          data.headTags.push({
-            tagName: 'script',
-            voidTag: false,
-            attributes: { src: `xxxx.js` },
-          });
-          console.log('data.headTags', data.headTags);
-          cb(null, data);
+      compilation.hooks.processAssets.tapAsync(
+        {
+          name: 'RouteMappingPlugin',
+          stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
         },
-      );
-    });
+        (assets, callback) => {
+          const routerFilePath = path.resolve(this.options.routerFilePath);
+          const outputFile = this.options.outputFile || 'route-mapping.js';
 
+          fs.readFile(routerFilePath, 'utf-8', (err, data) => {
+            if (err) {
+              return callback(err);
+            }
 
-    compiler.hooks.emit.tapAsync(
-      'RouteMappingPlugin',
-      (compilation, callback) => {
-        const routerFilePath = path.resolve(this.options.routerFilePath);
-        const outputFile = this.options.outputFile || 'route-mapping.js';
+            const ast = parser.parse(data, {
+              sourceType: 'module',
+              plugins: ['dynamicImport'],
+            });
 
-        fs.readFile(routerFilePath, 'utf-8', (err, data) => {
-          if (err) {
-            return callback(err);
-          }
+            const routeMappings: { componentName: string; importPath: string; }[] = [];
+            const dynamicImports = new Map();
 
-          const ast = parser.parse(data, {
-            sourceType: 'module',
-            plugins: ['dynamicImport'],
-          });
-
-          const routeMappings: RouteMapping[] = [];
-          const dynamicImports = new Map();
-
-          traverse(ast, {
-            ImportDeclaration({ node }) {
-              const importPath = node.source.value;
-              const componentName = node.specifiers[0].local.name;
-              routeMappings.push({ componentName, importPath });
-            },
-            CallExpression(path) {
-              if (
-                path.node.callee.type === 'Import' &&
-                path.node.arguments[0].type === 'StringLiteral'
-              ) {
-                const importPath = path.node.arguments[0].value;
-                const parentPath = path.findParent(
-                  (p) =>
-                    p.isObjectProperty() && (p.node.key as any).name === 'component',
-                );
-                if (parentPath) {
-                  dynamicImports.set(parentPath.node, importPath);
+            traverse(ast, {
+              ImportDeclaration({ node }) {
+                const importPath = node.source.value;
+                const componentName = node.specifiers[0].local.name;
+                routeMappings.push({ componentName, importPath });
+              },
+              CallExpression(path) {
+                if (
+                  path.node.callee.type === 'Import' &&
+                  path.node.arguments[0].type === 'StringLiteral'
+                ) {
+                  const importPath = path.node.arguments[0].value;
+                  const parentPath = path.findParent(
+                    (p) =>
+                      p.isObjectProperty() && (p.node.key as any).name === 'component',
+                  );
+                  if (parentPath) {
+                    dynamicImports.set(parentPath.node, importPath);
+                  }
                 }
-              }
-            },
-          });
+              },
+            });
 
-          const routes: { path?: string; component?: string }[] = [];
+            const routes: {}[] = [];
 
-          traverse(ast, {
-            Declaration(path) {
-              // 类型为VariableDeclaration且参数名为prefetchRoutes
-              if (path.node.type === 'VariableDeclaration' && (path.node.declarations[0].id as any).name === 'prefetchRoutes') {
-                if (path.node.declarations[0].init && path.node.declarations[0].init.type === 'ArrayExpression') {
-                  const prefetchRoutes = path.node.declarations[0].init.elements;
-                  (prefetchRoutes).forEach((routeNode: any) => {
-                    if (routeNode.type === 'ObjectExpression') {
-                      const route: { path?: string; component?: string } = {};
-                      routeNode.properties.forEach((prop: any) => {
-                        if (prop.key.name === 'path') {
-                          route.path = prop.value.value;
-                        }
-                        if (prop.key.name === 'component') {
-                          if (
-                            prop.value.type === 'ArrowFunctionExpression' &&
-                            dynamicImports.has(prop)
-                          ) {
-                            route.component = dynamicImports.get(prop);
-                          } else if (prop.value.type === 'Identifier') {
-                            const componentName = prop.value.name;
-                            const mapping = routeMappings.find(
-                              (m) => m.componentName === componentName,
-                            );
-                            if (mapping) {
-                              route.component = mapping.importPath;
+            traverse(ast, {
+              Declaration(path) {
+                if (
+                  path.node.type === 'VariableDeclaration' &&
+                  (path.node.declarations[0].id as any).name === 'prefetchRoutes'
+                ) {
+                  if (
+                    path.node.declarations[0].init &&
+                    path.node.declarations[0].init.type === 'ArrayExpression'
+                  ) {
+                    const prefetchRoutes =
+                      path.node.declarations[0].init.elements;
+                    prefetchRoutes.forEach((routeNode: any) => {
+                      if (routeNode.type === 'ObjectExpression') {
+                        const route: { path?: string; component?: string } = {};
+                        routeNode.properties.forEach((prop: any) => {
+                          if (prop.key.name === 'path') {
+                            route.path = prop.value.value;
+                          }
+                          if (prop.key.name === 'component') {
+                            if (
+                              prop.value.type === 'ArrowFunctionExpression' &&
+                              dynamicImports.has(prop)
+                            ) {
+                              route.component = dynamicImports.get(prop);
+                            } else if (prop.value.type === 'Identifier') {
+                              const componentName = prop.value.name;
+                              const mapping = routeMappings.find(
+                                (m) => m.componentName === componentName,
+                              );
+                              if (mapping) {
+                                route.component = mapping.importPath;
+                              }
                             }
                           }
+                        });
+                        if (route.path && route.component) {
+                          routes.push(route);
                         }
-                      });
-                      if (route.path && route.component) {
-                        routes.push(route);
                       }
-                    }
-                  });
+                    });
+                  }
                 }
-              }
-            }
-          });
-          const code = `window.__sdp_base_route_maps = ${JSON.stringify(routes)}`;
-          hashedFilename = replaceContentHashInFilename(outputFile, code)
-          compilation.assets[hashedFilename] = {
-            source: () => code,
-            size: () => code.length,
-          } as any;
+              },
+            });
 
-          callback();
-        });
-      },
-    );
+            const code = `window.__sdp_base_route_maps = ${JSON.stringify(
+              routes
+            )}`;
+
+            hashedFilename = replaceContentHashInFilename(outputFile, code);
+
+            // 将生成的代码注入到资源中
+            compilation.emitAsset(hashedFilename, {
+              source: () => code,
+              size: () => code.length,
+            } as any);
+
+            callback();
+          });
+        }
+      );
+    });
   }
 }
 
